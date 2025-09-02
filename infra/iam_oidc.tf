7f6f72985bc051330b827c7caa8dd6eed0580702
Artur Barreto <artur.gomes.barreto@gmail.com>
2025-09-02 21:45:42 +0200

    fix: missing actions in iam policy

--- infra/iam_oidc.tf
+++ infra/iam_oidc.tf
@@ -75,7 +75,8 @@ resource "aws_iam_role_policy" "github_actions_policy" {
           "iam:ListRolePolicies",
           "iam:GetRolePolicy",
           "iam:ListAttachedRolePolicies",
-          "iam:PutRolePolicy"
+          "iam:PutRolePolicy",
+          "iam:CreateServiceLinkedRole"
         ],
         Resource = "*"
       }

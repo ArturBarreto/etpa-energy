2ac49d441ebaf7318dcd55e810dd9b424677bfe5
Artur Barreto <artur.gomes.barreto@gmail.com>
2025-09-02 22:19:52 +0200

    fix: wrong arn of AWS managed policy

--- infra/iam_apprunner.tf
+++ infra/iam_apprunner.tf
@@ -16,5 +16,5 @@ resource "aws_iam_role" "apprunner_ecr_access" {
 # Attach AWS managed policy that grants ECR pull permissions
 resource "aws_iam_role_policy_attachment" "apprunner_ecr_access_attach" {
   role       = aws_iam_role.apprunner_ecr_access.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSAppRunnerServicePolicyForECRAccess"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }

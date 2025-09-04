120539e100473867d58e7fbde3673a05039064a0
Artur Barreto <artur.gomes.barreto@gmail.com>
2025-09-04 17:26:28 +0200

    fix: unused dto removed

--- src/main/java/com/etpa/energy/dto/ImportRequest.java
+++
@@ -1,9 +0,0 @@
-package com.etpa.energy.dto;
-
-import java.util.List;
-
-
-// For batch import of one meter-year (12 readings)
-public record ImportRequest(String meterId, String profileCode, int year, List<MeterReadingDTO> readings) {
-
-}
